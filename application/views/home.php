aa393b5e0a964c96876aa6461ddbffbaf3f2f4bd
yaolili <yaolili12235@gmail.com>
2015-12-29 16:12:57 +0800

    only need to change ajax url

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,5 @@
 <script src="<?= base_url('js/countLike.js') ?>"></script>
 
-<!-- OLD VERSION
-<div class="container" style="padding-bottom:50px;">
-	<div class="row" style="padding-bottom:30px;">
-        <div class="col-md-12" style="text-align:center;" >
-            <img src="images/logo.png" alt="ELER" />
-        </div>
-        <div class="col-md-12" style="text-align:center;" >
-            <img src="images/wa.jpg" alt="ELER" />
-            <textarea style="position: absolute; top:150px; width:570px; left: 250px; height:200px;" id="q" type="text"  name="q" placeholder="Input your text here"></textarea>
-            <span >
-                <button id="search-btn" type="submit" class="btn btn-default" style="position: absolute; top:150px; left: 820px; height:200px;" >EntityLinking</button>
-            </span>
-		</div>
-	</div>
-</div>
--->
-
-
 <div id="loginbox">
     <form id="search-form" class="form-vertical" role="search" action="<?= site_url('search/index') ?>" method="GET">
         <div class="control-group normal_text">
@@ -26,12 +8,7 @@
         <div class="control-group">
             <div class="controls">
                 <div class="main_input_box">
-                    <textarea id="q" name="q" type="text" style="height:150px; width:100%;">First documented in the 13th century, Berlin was the capital of the Kingdom of Prussia (1701–1918), the German Empire (1871–1918), the Weimar Republic (1919–33) and the Third Reich (1933–45). Berlin in the 1920s was the third largest municipality in the world. After World War II, the city became divided into East Berlin -- the capital of East Germany -- and West Berlin, a West German exclave surrounded by the Berlin Wall from 1961–89. Following German reunification in 1990, the city regained its status as the capital of Germany, hosting 147 foreign embassies.
-<<<<<<< HEAD
-                    Ayn_Rand, Allan_Dwan, Actrius,Algeria, Altruism, Alabama, Anarchism, Albedo
-
-=======
->>>>>>> 3e93aa80c0dce7ffd47667b96a30d09b27bcb417
+                    <textarea id="q" name="q" type="text" style="height:180px; width:100%;">First documented in the 13th century, Berlin was the capital of the Kingdom of Prussia (1701–1918), the German Empire (1871–1918), the Weimar Republic (1919–33) and the Third Reich (1933–45). Berlin in the 1920s was the third largest municipality in the world. After World War II, the city became divided into East Berlin. Ayn_Rand, Allan_Dwan, Actrius,Algeria, Altruism, Alabama, Anarchism, Albedo
                     </textarea>
                 </div>
             </div>
--- application/views/query.php
+++ application/views/query.php
@@ -1,6 +1,5 @@
 <script src="<?= base_url('js/jquery.min.js') ?>"></script>
 <script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
-<<<<<<< HEAD
 <script src="<?= base_url('js/jquery-ui.min.js') ?>"></script>
 <script src="<?= base_url('js/jquery.highlighttextarea.js') ?>"></script>
 <script src="<?= base_url('js/jquery-textrange.js') ?>"></script>
@@ -10,26 +9,11 @@
 
 <script type="text/javascript">
     var query = "<?php $query ?>";
-=======
-<script src="<?= base_url('js/tempo.min.js') ?>"></script>
-<script src="<?= base_url('js/lib/d3/d3.js') ?>"></script>
-<script src="<?= base_url('js/d3.layout.cloud.js') ?>"></script>
-<script type="text/javascript" src="<?= base_url('js/search.js') ?>"></script>
-<script src="http://echarts.baidu.com/build/dist/echarts.js"></script>
-<script type="text/javascript">
-    var query = "<?= $query ?>";
->>>>>>> 3e93aa80c0dce7ffd47667b96a30d09b27bcb417
 </script>
-
-
 
 <!--top-Header-menu-->
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 3e93aa80c0dce7ffd47667b96a30d09b27bcb417
 <div id="result-container" class="container" role="main">	
     <div class="row">
         <div class="col-md-1">
@@ -40,10 +24,7 @@
             </div>
             <div class="col-md-8">
                 <div style="text-align:right">
-                    <ul>
-                        <li class=""><a title="" href="#"><span class="text">系统设置</span></a></li>
-                        <li class=""><a title="" href="login.html"><span class="text">退出</span></a></li>
-                    </ul>
+                    <!-- here to add navigation if you like-->
                 </div>
             </div>
         </div>
@@ -57,24 +38,16 @@
             <div class="col-md-4">
                 <form id="search-form" role="search" action="<?= site_url('search/index') ?>" method="GET">
 
-<<<<<<< HEAD
                     <article class="main_input_box">
                         <textarea id="q" name="q" type="text" style="height:600px;width:100%"><?=$query ?>
                         </textarea>
                     </article>
-=======
-                    <div class="main_input_box">
-                        <textarea id="q" name="q" type="text" style="height:500px;width:100%"><? echo $query ?>
-                        </textarea>
-                    </div>
->>>>>>> 3e93aa80c0dce7ffd47667b96a30d09b27bcb417
                     <button class="btn btn-success" style="width:100%" type="submit">Search</button> 
 
                 </form> 
             </div>
-<<<<<<< HEAD
             <div id="allResult" class="col-md-8" style="background:white; height:600px">
-                <div id="info" style="height:30px">
+                <div id="info" style="height:50px">
                     <strong>Click Info:</strong>
                     <code>
                     <span class="line">{</span>
@@ -88,9 +61,6 @@
                 </div>
                 <div id="main" style="height:570px;">
                 </div>
-=======
-            <div class="col-md-8" style="background:white; height:500px">
->>>>>>> 3e93aa80c0dce7ffd47667b96a30d09b27bcb417
             </div>
         </div>
         <div class="col-md-1">
